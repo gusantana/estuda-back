6c588222b7ee778ba105d12a8f689e6f6dd56f53
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-23 14:02:40 -0400

    Adicionado classe que representa a requisição http

---
+++ core/class/Requisicao.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace core;
+
+class Requisicao
+{
+    protected $url;
+
+    public function __construct()
+    {
+        if (isset($_SERVER['PATH_INFO'])) {
+            $this->url  = $_SERVER['PATH_INFO'];
+        } else {
+            $this->url = '/';
+        }
+    }
+
+    public function get()
+    {
+        return $_GET;
+    }
+
+    public function post()
+    {
+        if (empty($_POST)) {
+            return json_decode(file_get_contents('php://input'), true);
+        }
+        return $_POST;
+    }
+
+    public function getUrl() 
+    {
+        return $this->url;
+    }
+}
