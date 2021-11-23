ecb80dd05e67b99c1581ab144845121cbd6e9530
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-23 14:03:07 -0400

    Adicionado classe que irá decodificar a rota da requisição

---
+++ core/class/DecodificadorRota.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace core;
+
+class DecodificadorRota
+{
+    private $rota;
+
+    function DecodificadorRota($umaRota = "/Aluno/")
+    {
+        $this->rota = $umaRota;
+    }
+
+    function obtemControlador()
+    {
+    }
+
+    function obtemMetodo()
+    {
+    }
+}
