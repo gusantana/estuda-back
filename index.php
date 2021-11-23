d12e03618300b5d7d705e8f1b47fa83a90a46585
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-23 14:02:00 -0400

    Adicionado diretórios de classes e interfaces na configuração

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ const REPOSITORIO_PASTA = "repository";
 define('SELF', pathinfo(__FILE__, PATHINFO_FILENAME));
 
 define('CORE_DIRETORIO', dirname(__FILE__).DIRECTORY_SEPARATOR.CORE_PASTA);
+define('CLASSE_CORE_DIRETORIO', dirname(__FILE__).DIRECTORY_SEPARATOR.'class'.DIRECTORY_SEPARATOR);
+define('INTERFACE_CORE_DIRETORIO', dirname(__FILE__).DIRECTORY_SEPARATOR.'interface'.DIRECTORY_SEPARATOR);
 
 define('SISTEMA_DIRETORIO', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
@@ -16,5 +18,7 @@ define('MODEL_DIRETORIO', dirname(__FILE__).DIRECTORY_SEPARATOR.MODELS_PASTA);
 define('REPOSITORIO_DIRETORIO', dirname(__FILE__).DIRECTORY_SEPARATOR.REPOSITORIO_PASTA);
 
 define('SYS_DIR', basename(CORE_DIRETORIO));
+
+ini_set("allow_url_fopen", true);
 
 require './core/Core.php';
