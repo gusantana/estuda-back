d44b8668a9bccb6b82c9c489347106821db99c35
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-23 20:38:35 -0400

    Implementação do despachador de requisição

--- core/class/DespachadorRequisicao.php
+++ core/class/DespachadorRequisicao.php
@@ -2,8 +2,21 @@
 
 namespace core;
 
+use Exception;
+
 class DespachadorRequisicao
 {
-
-    
+    public function __construct ($nomeDoControlador, $acao)
+    {
+        require(DIRETORIO_CONTROLADOR . DIRECTORY_SEPARATOR . $nomeDoControlador . '.php');
+        $reflector = new \ReflectionClass($nomeDoControlador);
+        $controlador = $reflector->newInstanceArgs(array());
+        if (method_exists($controlador, $acao)) {
+            $controlador->{$acao}(new Requisicao());
+        }
+        else {
+            http_response_code(404);
+            throw new Exception("404 - Não encontrado");
+        }
+    }
 }
