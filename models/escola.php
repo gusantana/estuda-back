f531bf875f61af729466b29521d1840e907b22a3
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-24 22:17:00 -0400

    Adaptado model Escola para herdar da classe Model

--- models/escola.php
+++ models/escola.php
@@ -2,9 +2,16 @@
 
 namespace models;
 
-class Escola
+include_once(DIRETORIO_MODEL . 'Model.php');
+
+class Escola extends Model
 {
     const tabela = 'escola';
+
+    public function __construct($dados = [])
+    {
+        parent::__construct($dados);
+    } 
 
     public $id;
     public $nome;
