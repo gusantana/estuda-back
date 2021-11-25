1d458a7ad5af63202e53515c355c8afcfca8b548
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-24 20:24:40 -0400

    Adicionado bean de Escola

---
+++ models/escola.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace models;
+
+class Escola
+{
+    public $id;
+    public $nome;
+    public $endereco;
+
+    
+
+    public $data_cadastro;
+    public $data_alterado;
+    public $data_excluido;
+}
