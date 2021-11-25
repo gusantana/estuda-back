9aa749aa1a66af34d4db6412a7c8bb5722455f80
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-24 21:43:17 -0400

    retornando escolas por id

--- controller/EscolaController.php
+++ controller/EscolaController.php
@@ -1,6 +1,9 @@
 <?php
 
 require(CLASSE_CORE_DIRETORIO . "Controller.php");
+require(DIRETORIO_REPOSITORIO . "EscolaRepositorio.php");
+       
+use repository\EscolaRepositorio;
 
 class EscolaController extends \core\Controller
 {
@@ -8,9 +11,14 @@ class EscolaController extends \core\Controller
     {
         parent::__construct();
     }
-    
+
     public function index()
     {
+        $escolaRepositorio = new EscolaRepositorio();
+        $id = $this->requisicao->get()['id'];
 
+        $resultado = $escolaRepositorio->getPorId($id);
+        return json_encode($resultado);
     }
 }
+  
