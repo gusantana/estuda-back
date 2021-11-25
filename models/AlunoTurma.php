b8eed0e4255aecce206cb50c6379e4915f59b4f4
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-25 19:10:01 -0400

    Adicionado Model e Repositório de AlunoTurma

---
+++ models/AlunoTurma.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace models;
+
+include_once(DIRETORIO_MODEL . 'Model.php');
+
+class AlunoTurma extends Model
+{
+    const tabela = 'aluno_turma';
+
+    public function __construct($dados = [])
+    {
+        parent::__construct($dados);
+    }
+
+    public $id;
+    public $id_aluno;
+    public $id_turma;
+
+    private $data_cadastro;
+    private $data_alterado;
+    private $data_excluido;
+}
