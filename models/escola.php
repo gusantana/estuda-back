c65bd276d0402bbe6cb7cb5df71777bb5f3b5a73
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-24 21:40:17 -0400

    Adicionado referencia da tabela escola

--- models/escola.php
+++ models/escola.php
@@ -4,11 +4,12 @@ namespace models;
 
 class Escola
 {
+    const tabela = 'escola';
+
     public $id;
     public $nome;
     public $endereco;
 
-    
 
     public $data_cadastro;
     public $data_alterado;
