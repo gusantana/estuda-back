515c3c4fd8762c8dc59ddf8ad3c6684e36d4d391
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-27 15:17:11 -0400

    Recuperando registros ativos e retornando o registro recentemente salvo

--- controller/EscolaController.php
+++ controller/EscolaController.php
@@ -3,7 +3,7 @@
 include_once(CLASSE_CORE_DIRETORIO . "Controller.php");
 include_once(DIRETORIO_REPOSITORIO . "EscolaRepositorio.php");
 include_once(DIRETORIO_MODEL . "Escola.php");
-       
+
 use repository\EscolaRepositorio;
 use \core\Controller;
 use \models\Escola;
@@ -18,8 +18,14 @@ class EscolaController extends Controller
     public function index()
     {
         $repositorio = new EscolaRepositorio();
-        $id = $this->requisicao->get()['id'];
+        $resultado = $repositorio->getAtivos();
+        return json_encode($resultado);
+    }
 
+    public function get()
+    {
+        $repositorio = new EscolaRepositorio();
+        $id = $this->requisicao->get()['id'];
         $resultado = $repositorio->getPorId($id);
         return json_encode($resultado);
     }
@@ -29,8 +35,9 @@ class EscolaController extends Controller
         $modelo = new Escola($this->requisicao->post());
         if ($modelo->validar()) {
             $repositorio = new EscolaRepositorio();
-            $repositorio->salvar($modelo);
+            $modelo = $repositorio->salvar($modelo);
         }
+        return json_encode($modelo);
     }
 
     public function excluir()
@@ -44,4 +51,3 @@ class EscolaController extends Controller
         }
     }
 }
-  
