0ee119bde1b97c53bf794afac3f05490baaafc82
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-29 15:28:47 -0400

    Adicionado busca de alunos não cadastrados em uma turma e de alunos ja cadastrados

--- repository/AlunoTurmaRepositorio.php
+++ repository/AlunoTurmaRepositorio.php
@@ -14,4 +14,45 @@ class AlunoTurmaRepositorio extends BaseRepositorio
         parent::__construct(new AlunoTurma());
     }
 
+
+    public function getAlunosNaoCadastradosNaTurma($id_turma)
+    {
+        $sql = "SELECT a.* from aluno a 
+where a.id not in (
+	select at2.id_aluno from aluno_turma at2 where at2.id_turma = :id_turma AND at2.data_excluido IS NULL
+) AND a.data_excluido is NULL";
+        $parametros = [];
+        $parametros[':id_turma'] = $id_turma;
+
+        $statement = $this->conexao->prepare($sql);
+        $statement->execute($parametros);
+
+        $resultado = [];
+        while ($tupla = $statement->fetch(\PDO::FETCH_ASSOC)) {
+            $resultado[] = $tupla;
+        }
+
+        return $resultado ?? [];
+    }
+
+
+    public function getAlunosDaTurma($id_turma)
+    {
+        $sql = "SELECT a.* FROM aluno a 
+INNER JOIN aluno_turma at1 on at1.id_aluno = a.id 
+where at1.id_turma = :id_turma and at1.data_excluido is null";
+
+        $parametros = [];
+        $parametros[':id_turma'] = $id_turma;
+
+        $statement = $this->conexao->prepare($sql);
+        $statement->execute($parametros);
+
+        $resultado = [];
+        while ($tupla = $statement->fetch(\PDO::FETCH_ASSOC)) {
+            $resultado[] = $tupla;
+        }
+
+        return $resultado ?? [];
+    }
 }
