cd1f30ea4793f4e57288fc110ae19819d4d580cb
Gustavo S. Coelho <gustavo_13roo@hotmail.com>
2021-11-29 18:31:27 -0400

    Adicionado query que busca as escolas com a quantidade de alunos

--- repository/EscolaRepositorio.php
+++ repository/EscolaRepositorio.php
@@ -14,5 +14,29 @@ class EscolaRepositorio extends BaseRepositorio
         parent::__construct(new Escola());
     }
 
-    
+    public function getEscolasComTotalDeAlunos()
+    {
+        $sql = "SELECT es.*, (
+            select count(0) from aluno 
+            inner join aluno_turma on aluno.id = aluno_turma.id_aluno 
+            inner join turma on turma.id = aluno_turma.id_turma 
+            where turma.id_escola = es.id 
+                and 
+	            aluno_turma.data_excluido is null
+                and
+                aluno.data_excluido is null
+            ) as quant_alunos
+        FROM ". $this->model::tabela . " es
+        WHERE es.data_excluido IS null";
+
+        $statement = $this->conexao->prepare($sql);
+        $statement->execute([]);
+
+        $resultado = [];
+        while ($tupla = $statement->fetch(\PDO::FETCH_ASSOC)) {
+            $resultado[] = $tupla;
+        }
+
+        return $resultado ?? [];
+    }
 }
